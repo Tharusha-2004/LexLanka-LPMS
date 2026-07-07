d1dc001878d753ef02e9b6bd849f9a109b57d371
Tharusha-2004 <tnethmina999@gmail.com>
2026-07-07 22:12:33 +0530

    update front end

---
+++ frontend/src/app/services/client.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClientService {
+  private apiUrl = 'http://localhost:3000/api/clients';
+
+  constructor(private http: HttpClient) { }
+
+  getAllClients(): Observable<any> {
+    return this.http.get<any>(this.apiUrl);
+  }
+}
