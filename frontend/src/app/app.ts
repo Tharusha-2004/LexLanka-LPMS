c505bea95822f3f99f7942a0cef618a6ce261358
Tharusha-2004 <tnethmina999@gmail.com>
2026-07-07 23:56:54 +0530

    frontend updated

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { ClientListComponent } from './components/client-list/client-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, ClientListComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
