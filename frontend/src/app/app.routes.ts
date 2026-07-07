ff88ec26dbe4bb4670bf86a80c6154d9f29fca0d
Tharusha-2004 <tnethmina999@gmail.com>
2026-07-07 23:46:08 +0530

    Building the Angular Navigation & Layout

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ClientListComponent } from './components/client-list/client-list.component';
+import { AddClientComponent } from './components/add-client/add-client.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/clients', pathMatch: 'full' },
+  { path: 'clients', component: ClientListComponent },
+  { path: 'add-client', component: AddClientComponent }
+];
